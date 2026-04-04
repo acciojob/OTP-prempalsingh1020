764cca9ea6738e165f568828f0fe822b9703fa49
Prempal Singh <prempalsingh030499@gmail.com>
2026-04-04 12:34:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,91 @@
 //your JS code here. If required.
+
+// let selectinputs = document.querySelectorAll(".code");
+
+// selectinputs[0].focus;
+
+// selectinputs.forEach((inputBox,ind)=>{
+// 	inputBox.addEventListener("input",(e)=>{
+// 	let currentBox = e.target.value;
+// 		if(currentBox == ""){
+// 			return 
+// 		}
+// 		if(ind<selectinputs.length-1){
+// 			selectinputs[ind+1].focus;
+// 		}
+		
+// 	})
+// 	selectinputs.addEventListener("keydown", (e) => {
+//         if (e.key === "Backspace" && !se.value && ind > 0) {
+//             inputs[index - 1].focus();
+//         }
+//     });
+// })
+
+
+
+
+let selectinputs = document.querySelectorAll(".code");
+
+// focus first input
+selectinputs[0].focus();
+
+selectinputs.forEach((inputBox, ind) => {
+
+    inputBox.addEventListener("input", (e) => {
+        let currentBox = e.target.value;
+
+        if (currentBox === "") return;
+
+       
+        if (ind < selectinputs.length - 1) {
+            selectinputs[ind + 1].focus();
+        }
+    });
+
+    inputBox.addEventListener("keydown", (e) => {
+        // move back on backspace if empty
+        if (e.key === "Backspace" && inputBox.value === "" && ind > 0) {
+            selectinputs[ind - 1].focus();
+        }
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
